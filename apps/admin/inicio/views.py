c03fd1c3619fc242e6828cf6602612c5161eb90f
Gustavo Leon <gust989@gmail.com>
2013-10-16 16:33:37 -0430

    Corregidas las rutas admin/front

--- apps/admin/inicio/views.py
+++ apps/admin/inicio/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 #from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.db.models import Count
 
 
